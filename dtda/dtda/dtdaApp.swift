885183fe4a3e1b53bc1c415fba5a456d57b971e4
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-06-18 01:47:19 +0900

    feat: initial commit

---
+++ dtda/dtda/dtdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dtdaApp.swift
+//  dtda
+//
+//  Created by Gojaehyun on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct dtdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dtda/dtdaTests/dtdaTests.swift
@@ -0,0 +1,17 @@
+//
+//  dtdaTests.swift
+//  dtdaTests
+//
+//  Created by Gojaehyun on 6/18/25.
+//
+
+import Testing
+@testable import dtda
+
+struct dtdaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
